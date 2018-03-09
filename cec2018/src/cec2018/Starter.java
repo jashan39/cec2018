078bd50f55fcb95d5848bc5f6c675bed3184ac88
Navjashan Singh <navjashansingh@gmail.com>
2018-03-09 12:30:30 -0500

    Parsing JSON works

--- cec2018/src/cec2018/Starter.java
+++ cec2018/src/cec2018/Starter.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.JSONObject;
 import org.json.JSONArray;
 
 
@@ -14,12 +15,21 @@ public class Starter {
 	
 	public static void main(String[] args) throws Exception{	
 		
+		String parameters = sendGet("parameters");
+		JSONObject json = new JSONObject(parameters);
+		JSONObject json1 = json.getJSONObject("parameters");
+		JSONObject json2 = json.getJSONObject("parameters");
+		Integer json3 = json2.getInt("lifetime");
+
+		System.out.println(json.toString());
+		System.out.println(json1.toString());
+		System.out.println(json3);
 		
 		sendGet("startup");
 	}
 	
 	// HTTP GET request
-	public static void sendGet(String api_call) throws Exception {
+	public static String sendGet(String api_call) throws Exception {
 
 		String url = " http://www.cec-2018.ca/mcp/" + api_call + "?token=184d00ac0bfb7c92a8fc37dcac5e26b8";
 				
@@ -45,7 +55,6 @@ public class Starter {
 
 		//print result
 		System.out.println(response.toString());
-	    JSONArray jsonarray = new JSONArray(response.toString());
-
+		return response.toString();
 	}
 }
